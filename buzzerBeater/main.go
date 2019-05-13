980eefc77e08992c00505d633e999e0669ff1dd6
tong <paul.tong.cs@gmail.com>
2019-05-12 23:47:22 -0400

    add user model

--- buzzerBeater/main.go
+++ buzzerBeater/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Username string
+}
 
 func main() {
+	// router, like a signal-slot, create a mapping relationship
+	// the route will trigger corresponding function
 	http.HandleFunc("/", sayHello)
 
+	// start listening to the port
 	http.ListenAndServe(":8888", nil)
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	user := User{Username: "tong"}
+	tpl, _ := template.ParseFiles("templates/index.html")
+	tpl.Execute(w, &user)
 }
