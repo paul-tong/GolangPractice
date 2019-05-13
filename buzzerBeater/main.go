dd543fcfac2649de909dc594615448c9e9443bef
tong <paul.tong.cs@gmail.com>
2019-05-13 13:35:56 -0400

    implemented templates

--- buzzerBeater/main.go
+++ buzzerBeater/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -9,17 +10,69 @@ type User struct {
 	Username string
 }
 
+type Post struct {
+	User // extend User struct, will have all the fields of User struct
+	Body string
+}
+
+type IndexViewModel struct {
+	Title string
+	User  // extend User struct
+	Posts []Post
+}
+
 func main() {
 	// router, like a signal-slot, create a mapping relationship
 	// the route will trigger corresponding function
-	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/", showHomePage)
+	http.HandleFunc("/posts", showAllPosts)
+	http.HandleFunc("/love", sayLove)
+	http.HandleFunc("/bye", sayBye)
 
 	// start listening to the port
 	http.ListenAndServe(":8888", nil)
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	user := User{Username: "tong"}
-	tpl, _ := template.ParseFiles("templates/index.html")
-	tpl.Execute(w, &user)
+// combine given data and content page with layout, then render the page to client
+func renderTemplates(w http.ResponseWriter, contentPagePath string, data interface{}) {
+	/*var allFiles []string{
+		"templates/index.html"
+		"templates/header.html"
+	}*/
+
+	tpl, _ := template.ParseFiles("templates/layout.html", contentPagePath)
+	tpl.ExecuteTemplate(w, "layout", data)
+}
+
+func showHomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("showHomePage\n")
+	user := User{Username: "Tong"}
+
+	renderTemplates(w, "templates/index.html", user)
+}
+
+func showAllPosts(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("showAllPosts\n")
+
+	u1 := User{Username: "tong"}
+	u2 := User{Username: "duo duo"}
+
+	posts := []Post{
+		Post{User: u1, Body: "I like you duo duo!"},
+		Post{User: u2, Body: "I like you 3000, peng tong"},
+	}
+
+	v := IndexViewModel{Title: "Homepage", User: u1, Posts: posts}
+
+	renderTemplates(w, "templates/allPosts.html", &v)
+}
+
+func sayLove(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("sayLove\n")
+	renderTemplates(w, "templates/love.html", "Tong")
+}
+
+func sayBye(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("sayBye\n")
+	renderTemplates(w, "templates/bye.html", "Tong")
 }
